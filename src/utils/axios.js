24483ea3d416487a16a48cb277052bfeea6b7602
kushal bansal <93420302+kusl007@users.noreply.github.com>
2025-01-16 13:57:42 +0545

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -5,8 +5,7 @@ const instance = axios.create({
   headers: {
     accept: "application/json",
     Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNGQ2ZjJmMTYyMThmNzZjZmQ0ZTgwNWNhYzZlN2M2YyIsIm5iZiI6MTczMjcxNTExMi44MjA4ODYxLCJzdWIiOiI2NzQ3MjBmOTMwMWZiNGY2YjYxN2E1ZDQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.BDxKaBKOPRRTz5JLeJKB8hANTIPLk9sqedGBqTuMV98",
-      // import.meta.env.VITE_REACT_APP_TOKEN
+      import.meta.env.VITE_REACT_APP_TOKEN
   },
 });
 
